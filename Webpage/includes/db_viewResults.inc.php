2f16fd51c1564443808fac2ff2cbee880edf9feb
Croip3 <croip3@gmail.com>
2020-09-25 16:08:13 +0200

    künstler suchbar

--- Webpage/includes/db_viewResults.inc.php
+++ Webpage/includes/db_viewResults.inc.php
@@ -1,22 +1,44 @@
-<?php 
+
+
+<?php
+
 class ViewResults extends SearchDB{
         public function __construct(){
             $datas = $this->searchData();
             if (is_array($datas) || is_object($datas)){
-                echo "#######################################gefundene Daten: <br>";
+                echo "gefundene Daten: <br>";
+
+                echo '<table>';
+                    echo '<tr>';
+                        echo '<td>Titel</td>';
+                        echo "<td>Kuenstler</td>";
+                        echo "<td>Url</td>";
+                        echo "<td>Genre</td>";
+                        echo "<td>Epoche</td>";
+                    echo "</tr>";
+
                 foreach($datas as $data){
-                    echo $data['id']." ";
-                    echo $data['Tempo']." ";
-                    echo $data['Genre']." ";
-                    echo $data['Uploaddatum']." ";
-                    echo $data['Laenge']." ";
-                    echo $data['Jahr']." ";
-                    echo $data['Tonart']." ";
-                    echo $data['Epoche']." ";
-                    echo $data['Titel']." ";
-                    echo $data['Url']." ";
-                    echo $data['Kuenstler_ID']."<br>";
+                    echo '<tr>';
+                        echo "<td>".$data['Titel']."</td>";
+                        echo "<td>".$data['name']."</td>";
+                        echo "<td><a href=".$data['Url'].">".$data['Url']."</a></td>";
+                        echo "<td>".$data['Genre']."</td>";
+                        echo "<td>".$data['Epoche']."</td>";
+                    echo "</tr>";
+
+                        /*echo $data['id']." ";
+                        echo $data['Tempo']." ";
+                        echo $data['Genre']." ";
+                        echo $data['Uploaddatum']." ";
+                        echo $data['Laenge']." ";
+                        echo $data['Jahr']." ";
+                        echo $data['Tonart']." ";
+                        echo $data['Epoche']." ";
+                        echo $data['Titel']." ";
+                        echo "<a href=".$data['Url'].">".$data['Url']."</a>";
+                        echo $data['name']."<br>";*/
                 }
+                echo "</table>";
             }else{
                 echo "keine Daten gefunden";
             }
--- Webpage/search.php
+++ Webpage/search.php
@@ -6,7 +6,10 @@
 
 <html>
     <head>
-
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="main.css">
+        <title>Musiksuchmaschine</title>
     </head>
     <body>
         <header>
