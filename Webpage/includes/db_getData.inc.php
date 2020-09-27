4167ed7397e0356e57a8beca8d8a7f81613535f4
Croip3 <croip3@gmail.com>
2020-09-27 17:46:38 +0200

    Website mit AJAX und ersten BAckend Filter funktions Tests

--- Webpage/includes/db_getData.inc.php
+++ Webpage/includes/db_getData.inc.php
@@ -1,10 +1,47 @@
 <?php 
 class SearchDB extends DatabaseConnection{
         public function searchData(){
-            $search = '%'.$_POST['search'].'%';
-            echo 'searchbar: '.$search.'<br>';
+            $search = '%'.$_GET['search'].'%';
            
-            $res = $this->connect()->query("SELECT * FROM musikstueck INNER Join kuenstler ON musikstueck.Kuenstler_ID = kuenstler.id WHERE musikstueck.Genre like '$search' OR musikstueck.Epoche like '$search' OR musikstueck.Titel like '$search' OR kuenstler.name like '$search' ");
+            $res = $this->connect()->query("SELECT * FROM musikstueck WHERE Genre like '$search' OR Epoche like '$search' OR Titel like '$search' OR Url LIKE '$search' OR Sonstiges LIKE '$search' ");
+            $num = $res->num_rows;
+            if($num > 0){
+                while($row = $res->fetch_assoc()){
+                    $data[] = $row;
+                }
+                return $data;
+                
+            }
+        }
+
+        public function getArtist($id) {
+            $res = $this->connect()->query("SELECT kuenstler.name FROM musikstueck JOIN kuenstler ON musikstueck.Kuenstler_ID = kuenstler.id WHERE musikstueck.id = $id");
+            $num = $res->num_rows;
+            if($num > 0){
+                while($row = $res->fetch_assoc()){
+                    $data[] = $row;
+                }
+                return $data;
+                
+            }
+        }
+
+        public function getInstruments($id) {
+            $res = $this->connect()->query("SELECT instrument.name FROM beinhaltet JOIN musikstueck ON musikstueck.id = beinhaltet.stueckid JOIN instrument ON beinhaltet.instrumentid = instrument.id WHERE musikstueck.id = $id");
+            $num = $res->num_rows;
+            if($num > 0){
+                while($row = $res->fetch_assoc()){
+                    $data[] = $row;
+                }
+                return $data;
+                
+            }
+        }
+
+        public function getTempo(){
+            $sql = "SELECT Tempo FROM `musikstueck` ORDER BY Tempo*1";
+            $res = $this->connect()->query($sql);
+
             $num = $res->num_rows;
             if($num > 0){
                 while($row = $res->fetch_assoc()){
